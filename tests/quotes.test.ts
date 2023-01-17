88353b533a46269671a73470758b0c9bdae49d7d
stv-beep <aleixalguero0@gmail.com>
2023-01-17 17:27:25 +0100

    test for the quotes with pagination

--- tests/quotes.test.ts
+++ tests/quotes.test.ts
@@ -17,6 +17,7 @@ describe('topic quotes', () => {
        expect(response.body).toHaveLength(quotes.length)
     })
 })
+
 describe('specific driver quotes', () => {
     test('Verstappen quotes are returned (brainyquote)', async () => {
         await api
@@ -42,12 +43,13 @@ describe('specific driver quotes', () => {
         const response = await api.get('/quotes/ecclestone')
         expect(response.body).toHaveLength(specificQuotes.length)
     })
-    test('Briatore quotes are returned (quotetab)', async () => {
+    test('Williams quotes are returned (quotetab)', async () => {
+        jest.setTimeout(30000);
         await api
-        .get('/quotes/briatore')
+        .get('/quotes/williams')
         .expect(200)
         .expect('Content-Type',/application\/json/)
-        const response = await api.get('/quotes/briatore')
+        const response = await api.get('/quotes/williams')
         expect(response.body).toHaveLength(specificQuotes.length)
     })
     test('Zhou quotes are returned (thesportsrush)', async () => {
@@ -58,7 +60,99 @@ describe('specific driver quotes', () => {
         const response = await api.get('/quotes/zhou')
         expect(response.body).toHaveLength(specificQuotes.length)
     })
-    
+})
+
+describe('specific driver quotes with pagination (pages exceeded)', () => {
+    const exceededPages = 99
+
+    test('Pagination: Verstappen quotes are returned (brainyquote)', async () => {
+        await api
+        .get(`/quotes/verstappen/p/${exceededPages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/verstappen/p/${exceededPages}`)
+        expect(response.body).toBe('Number of pages not valid. Try between 1-6.')
+    })
+    test('Pagination: Kvyat quotes are returned (quotes.net)', async () => {
+        await api
+        .get(`/quotes/kvyat/p/${exceededPages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/kvyat/p/${exceededPages}`)
+        expect(response.body).toBe('Number of pages not valid. Try 1 page.')
+    })
+    test('Pagination: Ecclestone quotes are returned (azquotes)', async () => {
+        await api
+        .get(`/quotes/ecclestone/p/${exceededPages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/ecclestone/p/${exceededPages}`)
+        expect(response.body).toBe('Number of pages not valid. Try between 1-3.')
+    })
+    test('Pagination: Williams quotes are returned (quotetab)', async () => {
+        jest.setTimeout(30000);
+        await api
+        .get(`/quotes/williams/p/${exceededPages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/williams/p/${exceededPages}`)
+        expect(response.body).toBe('Number of pages not valid. Try between 1-4.')
+    })
+    test('Pagination: Zhou quotes are returned (thesportsrush)', async () => {
+        await api
+        .get(`/quotes/zhou/p/${exceededPages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/zhou/p/${exceededPages}`)
+        expect(response.body).toBe('Number of pages not valid. Try 1 page.')
+    })
+})
+
+describe('specific driver quotes with pagination', () => {
+    const pages = 1
+    const quotesPerPage = 10
+
+    test('Pagination: Verstappen quotes are returned (brainyquote)', async () => {
+        await api
+        .get(`/quotes/verstappen/p/${pages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/verstappen/p/${pages}`)
+        expect(response.body).toHaveLength(quotesPerPage)
+    })
+    test('Pagination: Kvyat quotes are returned (quotes.net)', async () => {
+        await api
+        .get(`/quotes/kvyat/p/${pages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/kvyat/p/${pages}`)
+        expect(response.body).toHaveLength(specificQuotes.length)
+    })
+    test('Pagination: Ecclestone quotes are returned (azquotes)', async () => {
+        await api
+        .get(`/quotes/ecclestone/p/${pages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/ecclestone/p/${pages}`)
+        expect(response.body).toHaveLength(quotesPerPage)
+    })
+    test('Pagination: Williams quotes are returned (quotetab)', async () => {
+        jest.setTimeout(30000);
+        await api
+        .get(`/quotes/williams/p/${pages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/williams/p/${pages}`)
+        expect(response.body).toHaveLength(quotesPerPage)
+    })
+    test('Pagination: Zhou quotes are returned (thesportsrush)', async () => {
+        await api
+        .get(`/quotes/zhou/p/${pages}`)
+        .expect(200)
+        .expect('Content-Type',/application\/json/)
+        const response = await api.get(`/quotes/zhou/p/${pages}`)
+        expect(response.body).toHaveLength(specificQuotes.length)
+    })
 })
 
 /* to exit Jest properly */
